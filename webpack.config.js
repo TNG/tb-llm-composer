6631fc83c0987b042ba17947a409373f0641ef72
Christoph Lienhard <christoph.lienhard@tngtech.com>
2024-02-23 14:42:46 +0100

    Add webpack to properly bundle output files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: {
+    options: "./src/options.ts",
+    background: "./src/background.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
